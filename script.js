cc1c78c24d9847013e24788f7a8e40502d3a813b
Lokendrakushwah12 <Lokendrakushwah8051@gmail.com>
2023-08-07 12:28:59 +0530

    made custom cursor

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Loader
+const loader = document.querySelector(".loader");
 window.addEventListener("load", () => {
-    document.querySelector(".loader").classList.add("loader--hidden")
+    loader.classList.add("loader--hidden")
+})
+
+// cursor
+const cursor = document.querySelector(".cursor");
+const links = document.querySelectorAll("a");
+
+window.addEventListener("mousemove", (e) => {
+
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+
+    links.forEach(link => {
+        link.addEventListener("mouseover", () => {
+            cursor.classList.add("hover");
+        })
+        link.addEventListener("mouseout", () => {
+            cursor.classList.remove("hover");
+        })
+    })
 })
