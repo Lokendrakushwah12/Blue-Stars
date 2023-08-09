fd08a0e07820cacad65b376ab5df05c5f9ef0e44
Lokendrakushwah12 <Lokendrakushwah8051@gmail.com>
2023-08-09 22:08:13 +0530

    updated scroll-bar

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ window.addEventListener("load", () => {
     loader.classList.add("loader--hidden")
 })
 
-// cursor
+
+// Show and hide cursor
 const cursor = document.querySelector(".cursor");
 const links = document.querySelectorAll("a");
+const body = document.querySelector("body");
 
+body.addEventListener("mouseleave", function () {
+    cursor.classList.add("mouse-hidden");
+})
+body.addEventListener("mouseenter", function () {
+    cursor.classList.remove("mouse-hidden");
+})
+
+
+// cursor
 window.addEventListener("mousedown", () => {
-    console.log(10);
     cursor.classList.add("clicks");
 })
 
 window.addEventListener("mouseup", () => {
-    console.log(10);
     cursor.classList.remove("clicks");
 })
+
 
 window.addEventListener("mousemove", (e) => {
 
