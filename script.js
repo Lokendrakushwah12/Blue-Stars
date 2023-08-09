a5c91d85ae7ace6b859fd273d718dbc55982c5ba
Lokendrakushwah12 <Lokendrakushwah8051@gmail.com>
2023-08-09 19:50:40 +0530

    cursor fixed 2

--- script.js
+++ script.js
@@ -8,12 +8,24 @@ window.addEventListener("load", () => {
 const cursor = document.querySelector(".cursor");
 const links = document.querySelectorAll("a");
 
+window.addEventListener("mousedown", () => {
+    console.log(10);
+    cursor.classList.add("clicks");
+})
+
+window.addEventListener("mouseup", () => {
+    console.log(10);
+    cursor.classList.remove("clicks");
+})
+
 window.addEventListener("mousemove", (e) => {
 
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
 
+
     links.forEach(link => {
+
         link.addEventListener("mouseover", () => {
             cursor.classList.add("hover");
         })
