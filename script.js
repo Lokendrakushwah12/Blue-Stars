2b4f6dc889ae83c1be411c2b71dd8016b42f8256
Lokendrakushwah12 <Lokendrakushwah8051@gmail.com>
2023-08-09 19:26:10 +0530

    cursor fixed

--- script.js
+++ script.js
@@ -10,11 +10,8 @@ const links = document.querySelectorAll("a");
 
 window.addEventListener("mousemove", (e) => {
 
-    let x = e.pageX;
-    let y = e.pageY;
-
-    cursor.style.left = `${x}px`;
-    cursor.style.top = `${y}px`;
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
 
     links.forEach(link => {
         link.addEventListener("mouseover", () => {
