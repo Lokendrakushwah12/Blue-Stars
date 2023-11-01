493a19bbde47489247e9de5669f7969c810c33e4
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2023-11-02 01:33:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,28 @@ window.addEventListener("mousemove", (e) => {
         })
     })
 })
+
+
+// $(window).ready(function(){
+
+//   let mouseX = 0;
+//   let mouseY = 0;
+//   let xp = 0;
+//   let yp = 0;
+
+//   $(document).mousemove(function(e){
+//     $(".custom__cursor__inner").css({
+//       transform: 'translate(' + (e.clientX - 3.25) + 'px, ' + (e.clientY - 3.25) + 'px)'
+//     });
+
+//     mouseX = e.clientX - 10;
+//     mouseY = e.clientY - 10;
+//   });
+
+//   setInterval(function(){
+//     xp += ((mouseX - xp)/6);
+//     yp += ((mouseY - yp)/6);
+//     $(".custom__cursor__outer").css({transform: 'translateX('+ (xp - 15) +'px) translateY('+ (yp - 15) +'px)'} );
+//   }, 10);
+// })
+
